42e0b0dd8ae0ece307a337f8bdb3f4db359e30e1
Teivar <kirillzalesskiy@gmail.com>
2014-08-27 11:17:51 +0400

    Version 1
    27.08.2014

--- m1e4/Main.java
+++ m1e4/Main.java
@@ -1,8 +1,39 @@
 public class Main {
-	
+
 	public static House createHouse(){
-		// FIXME your code here
+		
+		House house = new.House;
+		house.address = "str1";
+		house.countOfFloors = 5;
+		house.appartmentsCount = 10;
+		house.owner = "Max";		
 		return house;
+
+	}
+
+	public static void main(String[] args){
+		House myHome = createHouse();
+		House myWork = createHouse();
+		
+		System.out.println(myHouse.owner + myHouse.address);
+		System.out.println(myWork.owner + myWork.address);
+
+		myHouse.address = "Tula, Hvorostuhina 11";
+		myHouse.countOfFloors = 9;
+		myHouse.appartmentsCount = 36;
+		myHouse.owner = "JK";
+
+		myWork.address = "Tula, Lenina 6";
+		myWork.countOfFloors = 11;
+		myWork.appartmentsCount = 213;
+		myWork.owner = "Zdravservice";
+
+		System.out.println(myHouse.owner + myHouse.address);
+		System.out.println(myWork.owner + myWork.address);
+
+		myHouse = null;
+		myWork = null;
+
 	}
 
 }
